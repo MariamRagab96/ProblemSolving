4a27084691f92e4613cfb6b999cc743424f2b7bd
MariamRagab96 <mariamragab.ieee@gmail.com>
2021-06-25 18:54:00 -0700

    Update the solution of Two-Number Problem from Leet Code Online Judge

--- Leetcode/twoNumber/Solution.java
+++ Leetcode/twoNumber/Solution.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Leetcode.twoNumber;
 
-/**
- *
- * @author pc
- */
+import java.math.BigDecimal;
+
+// Definition for singly-linked list.
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution {
-    
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = null;
+        String first = "", second = "";
+        while (l1 != null) { //O(n)
+            first = l1.val + first;
+            l1 = l1.next;
+        }
+        while (l2 != null) { //O(n)
+            second = l2.val + second;
+            l2 = l2.next;
+        }
+        BigDecimal num1 = new BigDecimal(first);
+        BigDecimal num2 = new BigDecimal(second);
+        String num12 = num1.add(num2).toString();
+        System.out.println(num12);
+
+        for (int i = 0; i < num12.length(); i++) { //insert first in linkedList
+            ListNode node = new ListNode((Integer.parseInt(num12.substring(i, i + 1))));
+            node.next = res;
+            res = node;
+        }
+        return res;
+    }
 }
